51475f05516e9798548b3a52c240c3f42b6ca310
Robey Pointer <robey@twitter.com>
2010-09-02 16:32:25 -0700

    why is this so complicated?

--- lib/gizzard/hash.rb
+++ lib/gizzard/hash.rb
@@ -4,7 +4,7 @@ module Gizzard
       prime = 1099511628211
       rv = 0xcbf29ce484222325
       data.each_byte do |byte|
-        rv = ((rv ^ (byte.ord & 0xff)) * prime) & 0xffffffffffffffff
+        rv = ((rv ^ (byte & 0xff)) * prime) & 0xffffffffffffffff
       end
       # trim to 60 bits for gizzard.
       rv & 0x0fffffffffffffff
