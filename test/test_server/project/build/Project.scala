5e2267953b6e47fe0e177182e3ad639d59bb2a08
Stu Hood <stuhood@twitter.com>
2012-03-14 21:45:11 -0700

    Expect ordered subtrees

--- test/test_server/project/build/Project.scala
+++ test/test_server/project/build/Project.scala
@@ -7,7 +7,7 @@ class GizzmoServerProject(info: ProjectInfo) extends StandardProject(info) {
   override def filterScalaJars = false
 
   val scalaTools = "org.scala-lang" % "scala-compiler" % "2.7.7"
-  val gizzard    = "com.twitter" % "gizzard" % "3.0.0-beta35-SNAPSHOT-stuhood"
+  val gizzard    = "com.twitter" % "gizzard" % "3.0.5-SNAPSHOT-stuhood"
   val querulous  = "com.twitter" % "querulous" % "2.6.5"
 
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
